34d511c89c911ae33e2b45dff9d1cc9e0d1e5266
SteamMachina <52200491+SteamMachina@users.noreply.github.com>
2022-04-13 10:18:21 +0200

    Update generate_grid.c
    Changing appearance of grids

--- generate_grid.c
+++ generate_grid.c
@@ -7,28 +7,32 @@
 // Display a 4 x 4 array
 void display_the_grid_4x4(int list[4][4], int col, int row)
 {
-    printf("__ __ __ __ __\n");
+    printf("=================\n");
     for(int i = 0; i < row; i++)
     {
+        printf("| ");
         for(int j = 0; j < col; j++)
         {
             printf("%d | ",list[i][j]);
         }
         printf("\n");
     }
-    printf("__ __ __ __ __\n");
+    printf("=================\n");
 }
 
 // Display a 8 x 8 array
 
 void display_the_grid_8x8(int list[8][8], int col, int row)
 {
+    printf("==================================\n");
     for(int i = 0; i < row; i++)
     {
+        printf("| ");
         for(int j = 0; j < col; j++)
         {
-            printf("%d |",list[i][j]);
+            printf("%d | ",list[i][j]);
         }
         printf("\n");
     }
+    printf("==================================\n");
 }
