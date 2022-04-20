943146e59e2fa14079f809a9f09ca80ad4862f13
Rolwen <n.rnolwen@gmail.com>
2022-04-20 15:48:47 +0200

    update main file

--- main.c
+++ main.c
@@ -48,16 +48,26 @@ int main() {
     scanf(" %d", &option);
 
     switch(option){
+        case 0:
+        {
+            break;
+        }
         case 1:
+        {
             printf("1");
             break;
+        }
         case 2:
+        {
             printf("2");
             break;
+        }
         case 3:
+        {
             printf("3");
             break;
+        }
         default:
-            printf("default");
+            printf("Choice selected not proposed.");
     }
 }
