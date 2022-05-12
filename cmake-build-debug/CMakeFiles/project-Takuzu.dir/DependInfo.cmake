afd0c216cb898dce97d5ccdc11f0e3d30f691732
SteamMachina <lisaminne123@gmail.com>
2022-05-12 17:25:25 +0200

    added comments

---
+++ cmake-build-debug/CMakeFiles/project-Takuzu.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/minne/Documents/GitHub/project-Takuzu/generate_grid.c" "CMakeFiles/project-Takuzu.dir/generate_grid.c.obj" "gcc" "CMakeFiles/project-Takuzu.dir/generate_grid.c.obj.d"
+  "C:/Users/minne/Documents/GitHub/project-Takuzu/hints_functions.c" "CMakeFiles/project-Takuzu.dir/hints_functions.c.obj" "gcc" "CMakeFiles/project-Takuzu.dir/hints_functions.c.obj.d"
+  "C:/Users/minne/Documents/GitHub/project-Takuzu/main.c" "CMakeFiles/project-Takuzu.dir/main.c.obj" "gcc" "CMakeFiles/project-Takuzu.dir/main.c.obj.d"
+  "C:/Users/minne/Documents/GitHub/project-Takuzu/menus.c" "CMakeFiles/project-Takuzu.dir/menus.c.obj" "gcc" "CMakeFiles/project-Takuzu.dir/menus.c.obj.d"
+  "C:/Users/minne/Documents/GitHub/project-Takuzu/settings.c" "CMakeFiles/project-Takuzu.dir/settings.c.obj" "gcc" "CMakeFiles/project-Takuzu.dir/settings.c.obj.d"
+  "C:/Users/minne/Documents/GitHub/project-Takuzu/solving_grid.c" "CMakeFiles/project-Takuzu.dir/solving_grid.c.obj" "gcc" "CMakeFiles/project-Takuzu.dir/solving_grid.c.obj.d"
+  "C:/Users/minne/Documents/GitHub/project-Takuzu/validity_functions.c" "CMakeFiles/project-Takuzu.dir/validity_functions.c.obj" "gcc" "CMakeFiles/project-Takuzu.dir/validity_functions.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/project-Takuzu.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project-Takuzu.dir/generate_grid.c.obj"
+  "CMakeFiles/project-Takuzu.dir/generate_grid.c.obj.d"
+  "CMakeFiles/project-Takuzu.dir/hints_functions.c.obj"
+  "CMakeFiles/project-Takuzu.dir/hints_functions.c.obj.d"
+  "CMakeFiles/project-Takuzu.dir/main.c.obj"
+  "CMakeFiles/project-Takuzu.dir/main.c.obj.d"
+  "CMakeFiles/project-Takuzu.dir/menus.c.obj"
+  "CMakeFiles/project-Takuzu.dir/menus.c.obj.d"
+  "CMakeFiles/project-Takuzu.dir/settings.c.obj"
+  "CMakeFiles/project-Takuzu.dir/settings.c.obj.d"
+  "CMakeFiles/project-Takuzu.dir/solving_grid.c.obj"
+  "CMakeFiles/project-Takuzu.dir/solving_grid.c.obj.d"
+  "CMakeFiles/project-Takuzu.dir/validity_functions.c.obj"
+  "CMakeFiles/project-Takuzu.dir/validity_functions.c.obj.d"
+  "libproject-Takuzu.dll.a"
+  "project-Takuzu.exe"
+  "project-Takuzu.exe.manifest"
+  "project-Takuzu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/project-Takuzu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
